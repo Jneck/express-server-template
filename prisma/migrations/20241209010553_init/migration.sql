7da4766a4065f44b48baed94aaeed2d233601bb4
ChanyoungPark59 <126754788+ChanyoungPark59@users.noreply.github.com>
2024-12-09 10:34:45 +0900

    prisma 적용

---
+++ prisma/migrations/20241209010553_init/migration.sql
@@ -0,0 +1,30 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `authorId` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `published` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `title` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the `Profile` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+  - Made the column `content` on table `Post` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Profile" DROP CONSTRAINT "Profile_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "Post" DROP COLUMN "authorId",
+DROP COLUMN "published",
+DROP COLUMN "title",
+ADD COLUMN     "sharedCount" INTEGER NOT NULL DEFAULT 0,
+ADD COLUMN     "views" INTEGER NOT NULL DEFAULT 0,
+ALTER COLUMN "content" SET NOT NULL;
+
+-- DropTable
+DROP TABLE "Profile";
+
+-- DropTable
+DROP TABLE "User";
---
+++ src/index.ts
@@ -0,0 +1,58 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+import express from "express";
+// postgres 연결
+const prisma = new PrismaClient({
+  errorFormat: "pretty",
+});
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+app.get("/posts", async (req, res) => {
+  const posts = await prisma.post.findMany();
+  res.json(posts);
+});
+
+app.get(`/posts/:id`, async (req, res) => {
+  const { id }: { id?: string } = req.params;
+
+  const post = await prisma.post.findUnique({
+    where: { id: Number(id) },
+  });
+  res.json(post);
+});
+
+app.post("/posts", async (req, res) => {
+  await prisma.post.create({
+    data: {
+      content: "Alice",
+    },
+  });
+
+  const posts = await prisma.post.findMany();
+  console.log(posts);
+});
+
+app.patch("/posts/:id/views", async (req, res) => {
+  const { id }: { id?: string } = req.params;
+
+  const post = await prisma.post.update({
+    where: { id: Number(id) },
+    data: { views: { increment: 1 } },
+  });
+  console.log(post);
+});
+
+app.delete("/posts/:id", async (req, res) => {
+  const { id }: { id?: string } = req.params;
+
+  const post = await prisma.post.delete({
+    where: { id: Number(id) },
+  });
+  console.log(post);
+});
+
+app.listen(port, () => {
+  console.log(`
+🚀 Server ready at: http://localhost:${port}`);
+});
